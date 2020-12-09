b39939689c40eb610e1d59dec7a601e2a72cf7ee
cwesterink <constantine.westerink@gmail.com>
2020-12-09 09:28:27 -0800

    adds blueprints

---
+++ chat.py
@@ -0,0 +1,82 @@
+from flask import Blueprint render_template, redirect, request, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import random as rand
+
+chat = Blueprint('chat', __name__, static_folder='static', template_folder='templates')
+
+
+
+@chat.route('')
+
+
+@chat.route('/create',methods = ["POST","GET"])
+def createChat():
+    if log():
+        session['code'] = rand.randint(1000, 9999)
+
+        db.session.add(rooms(code=session['code'], msg=session['user']+ " entered the chat.",members=1))
+        db.session.commit()
+        return redirect(url_for('room'))
+    else:
+        return redirect(url_for('home'))
+@chat.route('/join',methods=["GET","POST"])
+def joinChat():
+    if log():
+        if request.method == "GET":
+            return render_template("join.html")
+        else:
+            session['code'] = request.form['code']
+
+            myRoom = rooms.query.filter_by(code=session['code']).first()
+            if myRoom is None:
+
+                flash("room not found")
+                return render_template("join.html")
+            else:
+                myRoom.members += 1
+                myRoom.msg += ","+session['user']+ " entered the chat."
+                db.session.commit()
+                return redirect(url_for('room'))
+    else:
+        return "user ny sign"
+
+@chat.route("/room", methods = ["GET","POST"])
+def room():
+    myRoom = rooms.query.filter_by(code=session['code']).first()
+    if request.method == "POST":
+        print(request.form)
+        print(len(request.form))
+        if len(request.form) == 0:
+            myRoom.msg +=","+session['user']+ " left the chat."
+            myRoom.members -= 1
+            if myRoom.members == 0:
+                db.session.delete(myRoom)
+            db.session.commit()
+            return redirect(url_for('home'))
+        else:
+            myRoom.msg += ","+session['user']+":  "+request.form['chat']
+            db.session.commit()
+
+
+    msgs = myRoom.msg.split(',')
+
+    for i in msgs:
+        flash(i)
+
+    return render_template('chat.html',code = session['code'],user=session['user'],members = myRoom.members)
+@chat.route('/refresh', methods=['GET',"POST"])
+def refresh():
+    myRoom = rooms.query.filter_by(code=session['code']).first()
+    msgs = myRoom.msg.split(',')
+
+    for i in msgs:
+        flash(i)
+    return render_template('chat.html', code=session['code'], user=session['user'], members=myRoom.members)
+
+@chat.route("/joinRandom")
+def random():
+    myRoom = rooms.query.first()
+    myRoom.msg += "," + session['user'] + " entered the chat."
+    myRoom.members += 1
+    db.session.commit()
+    return redirect(url_for('room'))
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, redirect, request, url_for, session, f
 from flask_sqlalchemy import SQLAlchemy
 import random as rand
 app = Flask(__name__)
+
+app.register_blueprint(chat, url_prefix='/chat')
+
+
 app.secret_key = "const"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQlALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -57,77 +61,7 @@ def login():
         session['user'] = request.form['name']
         return redirect(url_for('home'))
 
-@app.route('/create',methods = ["POST","GET"])
-def createChat():
-    if log():
-        session['code'] = rand.randint(1000, 9999)
 
-        db.session.add(rooms(code=session['code'], msg=session['user']+ " entered the chat.",members=1))
-        db.session.commit()
-        return redirect(url_for('room'))
-    else:
-        return redirect(url_for('home'))
-@app.route('/join',methods=["GET","POST"])
-def joinChat():
-    if log():
-        if request.method == "GET":
-            return render_template("join.html")
-        else:
-            session['code'] = request.form['code']
-
-            myRoom = rooms.query.filter_by(code=session['code']).first()
-            if myRoom is None:
-
-                flash("room not found")
-                return render_template("join.html")
-            else:
-                myRoom.members += 1
-                myRoom.msg += ","+session['user']+ " entered the chat."
-                db.session.commit()
-                return redirect(url_for('room'))
-    else:
-        return "user ny sign"
-
-@app.route("/room", methods = ["GET","POST"])
-def room():
-    myRoom = rooms.query.filter_by(code=session['code']).first()
-    if request.method == "POST":
-        print(request.form)
-        print(len(request.form))
-        if len(request.form) == 0:
-            myRoom.msg +=","+session['user']+ " left the chat."
-            myRoom.members -= 1
-            if myRoom.members == 0:
-                db.session.delete(myRoom)
-            db.session.commit()
-            return redirect(url_for('home'))
-        else:
-            myRoom.msg += ","+session['user']+":  "+request.form['chat']
-            db.session.commit()
-
-
-    msgs = myRoom.msg.split(',')
-
-    for i in msgs:
-        flash(i)
-
-    return render_template('chat.html',code = session['code'],user=session['user'],members = myRoom.members)
-@app.route('/refresh', methods=['GET',"POST"])
-def refresh():
-    myRoom = rooms.query.filter_by(code=session['code']).first()
-    msgs = myRoom.msg.split(',')
-
-    for i in msgs:
-        flash(i)
-    return render_template('chat.html', code=session['code'], user=session['user'], members=myRoom.members)
-
-@app.route("/rand")
-def random():
-    myRoom = rooms.query.first()
-    myRoom.msg += "," + session['user'] + " entered the chat."
-    myRoom.members += 1
-    db.session.commit()
-    return redirect(url_for('room'))
 
 
 
