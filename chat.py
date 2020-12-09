9d1aa4a50acb778689b15177ed1f0226aba955dd
cwesterink <constantine.westerink@gmail.com>
2020-12-09 10:54:42 -0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,4 +1,4 @@
-from flask import Blueprint render_template, redirect, request, url_for, session, flash
+from flask import Blueprint, render_template, redirect, request, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import random as rand
 
