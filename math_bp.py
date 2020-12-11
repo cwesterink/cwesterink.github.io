153d74da266e98bf2eefafd9cbbcf7d1fb0978c3
cwesterink <constantine.westerink@gmail.com>
2020-12-11 10:01:43 -0800

    test path

--- math_bp.py
+++ math_bp.py
@@ -1,14 +1,22 @@
-from flask import Blueprint, render_template, redirect, request, url_for, session, flash
+import base64
 
-math_bp = Blueprint('math_bp', __name__, static_folder='static', template_folder='templates')
+from flask import Blueprint, render_template, redirect, request, url_for, session, flash, make_response, Response
+
 import matplotlib.pyplot as plt
+import io
 
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+# import math functions
 from calculator import mathy
 from calculator import fx
 
+math_bp = Blueprint('math_bp', __name__, static_folder='static', template_folder='templates')
+
 
 @math_bp.route('/calculator', methods=['POST', 'GET'])
-def clac():
+def calc():
     if request.method == 'POST':
         session['calcInpt'] = request.form['input']
         num = mathy(session['calcInpt'])
@@ -23,12 +31,27 @@ def function():
         session['calcInpt'] = request.form['input']
         print(session['calcInpt'])
         x, y = fx(session['calcInpt'])
-        flash(x)
-        flash(y)
-        plt.scatter(x, y)
+        flash(session['calcInpt'])
 
-        plt.grid()
-        #plt.show()
-        plt.savefig('/static/plt.png')  # save the figure to file
-        plt.close(plt)
+
+        # Generate plot
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.set_title("f(x)")
+        axis.set_xlabel("x")
+        axis.set_ylabel("y")
+        axis.grid()
+        axis.plot(x, y, "bo")
+
+        # Convert plot to PNG image
+        pngImage = io.BytesIO()
+        FigureCanvas(fig).print_png(pngImage)
+
+        # Encode PNG image to base64 string
+        image = "data:image/png;base64,"
+        image += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+        return render_template('calculator.html', math='Function', inptTxt='Enter Expression Below', extra='f(x)=',
+                               plt=image)
+
     return render_template('calculator.html', math='Function', inptTxt='Enter Expression Below', extra='f(x)=')
